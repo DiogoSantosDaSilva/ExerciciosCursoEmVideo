05a33ae24a3ce71603788f10bc410e712224baf3
Diogo Santos <diogosantosdasilva364@gmail.com>
2023-03-20 00:26:39 -0300

    Exercício de Soma
    Aqui eu adicionei cores ao terminal

---
+++ ex003.py
@@ -0,0 +1,5 @@
+numero1 = int(input('Digite um número: '))
+numero2 = int(input('Digite outro número: '))
+
+soma = numero1 + numero2
+print('\033[1;32ma soma entre {} e {} é {}.\033[m'.format(numero1, numero2, soma))
