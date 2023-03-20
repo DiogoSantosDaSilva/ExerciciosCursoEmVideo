69e60740711931a431645775ebfdc7fbe8c9ffc4
Diogo Santos <diogosantosdasilva364@gmail.com>
2023-03-20 01:29:06 -0300

    Uma atualização e um exercício
    No exercício 003 usamos a função is.numeric e etc.

--- ex003.py
+++ ex003.py
@@ -1,5 +1,15 @@
-numero1 = int(input('Digite um número: '))
-numero2 = int(input('Digite outro número: '))
+# Abaixo a minha resolução
 
-soma = numero1 + numero2
-print('\033[1;32ma soma entre {} e {} é {}.\033[m'.format(numero1, numero2, soma))
+# numero1 = int(input('Digite um número: '))
+# numero2 = int(input('Digite outro número: '))
+
+# soma = numero1 + numero2
+# print('\033[1;32ma soma entre {} e {} é {}.\033[m'.format(numero1, numero2, soma))
+
+# Agora é a resolução do professor
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+
+s = n1 + n2
+print('A soma entre {} e {} é igual a {}!'.format(n1, n2, s))
---
+++ ex004.py
@@ -0,0 +1,25 @@
+# A resolução abaixo é minha
+
+# valor = input('Digite algo: ')
+# print('O tipo primitivo desse valor é \033[1;30;47m{}\033[m'.format(type(valor)))
+# print('Só tem espaços? \033[1;31;46m{}\033[m'.format(valor.isspace()))
+# print('É um número? \033[1;32;45m{}\033[m'.format(valor.isnumeric()))
+# print('É alfabético? \033[1;33;44m{}\033[m'.format(valor.isalpha()))
+# print('É alfanúmerico? \033[1;34;43m{}\033[m'.format(valor.isalnum()))
+# print('Está em muiúsculas? \033[1;35;42m{}\033[m'.format(valor.isupper()))
+# print('Está em minúsculas? \033[1;36;41m{}\033[m'.format(valor.islower()))
+# print('Está capitalizada? \033[1;37;40{}\033[m'.format(valor.istitle()))
+
+# baixo a resolução do prefessor, 
+# esse exercício não esta formatado
+
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é', type(a))
+print('Só tem espaços?', a.isspace())
+print('É um número?', a.isnumeric())
+print('É alfabético?', a.isalpha())
+print('É alfanúmerico?', a.isalnum())
+print('Está em maiúsculas?', a.isupper())
+print('Está em minúsculas?', a.islower())
+print('Está capitalizada?', a.istitle())
+
